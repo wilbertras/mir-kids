30896180daa24483386923d76728e4c9d0e86c1b
wilbertr <125577618+wilbertr@users.noreply.github.com>
2025-02-11 14:35:08 +0100

    make plot of spectra

--- figures/transmission_spectra.py
+++ figures/transmission_spectra.py
@@ -1,3 +1,144 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import scipy.constants as sc
+from scipy.integrate import cumulative_trapezoid
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from filters.filters import load_all_filters
 
+def planck_wl(wl, t):
+    return sc.h * sc.c**2 / (wl**5 * (np.exp(sc.h * sc.c / (wl * sc.k * t)) - 1))
+
+filters = load_all_filters()
+
+wl = filters['wl']*1e6
+bp38 = filters['bp38']
+bp85 = filters['bp85']
+bp185 = filters['bp185']
+caf2 = filters['caf2']
+znse = filters['znse']
+nd1 = filters['nd1']
+nd2 = filters['nd2']
+nd3 = filters['nd3']
+ger = filters['ger'] 
+bp25 = filters['bp25']
+sp_a = filters['sp_a']
+sp_b = filters['sp_b']
+lp = filters['lp']
+
+tot38 = bp38**4*caf2**4*ger*nd2*nd3
+tot85 = bp85**4*caf2**4*ger*nd2*nd3
+tot185 = bp185**5*nd1*nd3*znse**2
+tot25 = bp25**3*lp*sp_a**3*sp_b
+
+bb300 = planck_wl(wl*1e-6, 300)*1e-6
+bb160 = planck_wl(wl*1e-6, 160)*1e-6
+bb40 = planck_wl(wl*1e-6, 40)*1e-6
+
+fig, axes = plt.subplot_mosaic('ab;cd;ef', figsize=(12,6), constrained_layout=True)
+xlim = [1, 100]
+ax = axes['a']
+ax.semilogx(wl, bp38, label='BP38')
+ax.fill_between(wl, bp38, 0, where=(wl < 45), alpha=0.3)
+ax.legend()
+ax.set_ylim([0,1])
+ax.set_xlim(xlim)
+ax.set_xlabel('Wavelength [µm]')
+ax.set_ylabel('Transmission [-]')
+
+ax = axes['b']
+ax.semilogx(wl, bp85, label='BP85', zorder=1)
+ax.fill_between(wl, bp85, 0, where=(wl < 45), alpha=0.3)
+ax.semilogx(wl, filters['caf2'], label='CaF_2', zorder=0)
+ax.fill_between(wl, caf2, 0, where=(wl < 16), alpha=0.3)
+ax.legend()
+ax.set_ylim([0,1])
+ax.set_xlim(xlim)
+ax.set_xlabel('Wavelength [µm]')
+ax.set_ylabel('Transmission [-]')
+
+ax = axes['c']
+ax.semilogx(wl, bp185, label='BP185', zorder=1)
+ax.fill_between(wl, bp185, 0, where=(wl < 40), alpha=0.3)
+ax.semilogx(wl, znse, label='ZnSe', zorder=0)
+ax.fill_between(wl, znse, 0, where=(wl < 25), alpha=0.3)
+ax.legend()
+ax.set_ylim([0,1])
+ax.set_xlim(xlim)
+ax.set_xlabel('Wavelength [µm]')
+ax.set_ylabel('Transmission [-]')
+
+ax = axes['d']
+ax.semilogx(wl, bp25, label='BP25', zorder=3)
+ax.fill_between(wl, bp25, 0, where=((wl < 77) & (wl > 1.67)), alpha=0.3)
+ax.semilogx(wl, sp_a, label='SP1', zorder=2)
+ax.fill_between(wl, sp_a, 0, where=((wl < 77)&(wl > 1.67)), alpha=0.3)
+ax.semilogx(wl, sp_b, label='SP2', zorder=1)
+ax.fill_between(wl, sp_b, 0, where=((wl < 77)&(wl > 1.67)), alpha=0.3)
+ax.semilogx(wl, lp, label='LP', zorder=0)
+ax.fill_between(wl, lp, 0, where=((wl < 82)&(wl > 1.67)), alpha=0.3)
+ax.legend()
+ax.set_ylim([0,1])
+ax.set_xlim(xlim)
+ax.set_xlabel('Wavelength [µm]')
+ax.set_ylabel('Transmission [-]')
+
+ax = axes['e']
+ax.loglog(wl, nd1, label='ND1')
+ax.fill_between(wl, nd1, 0, where=(wl < 18), alpha=0.3)
+ax.loglog(wl, nd2, label='ND2')
+ax.fill_between(wl, nd2, 0, where=(wl < 18), alpha=0.3)
+ax.loglog(wl, nd3, label='ND3')
+ax.fill_between(wl, nd3, 0, where=(wl < 18), alpha=0.3)
+ax.legend()
+ax.set_ylim([1e-4, 1e0])
+ax.set_xlim(xlim)
+ax.set_xlabel('Wavelength [µm]')
+ax.set_ylabel('Transmission [-]')
+
+
+fig, axes = plt.subplot_mosaic('ab;cd', figsize=(8,5), constrained_layout=True, sharey=True, sharex=True)
+ax = axes['a']
+ax.loglog(wl, tot38*bb300, label='3.8 µm')
+ax.fill_between(wl, tot38*bb300, 0, alpha=0.3)
+ax.loglog(wl, bb300, c='k', label='Planck 300 K')
+cumulative_tot38_bb300 = np.cumsum(tot38 * bb300*np.diff(wl, prepend=wl[0]))
+# cumulative_tot38_bb300 = cumulative_trapezoid(tot38 * bb300, wl, initial=None)
+ax.loglog(wl, cumulative_tot38_bb300, label='Cumulative power')
+ax.legend()
+ax.set_ylim([1e-20, 1e1])
+ax.set_xlim([1,100])
+ax.set_xlabel('Wavelength [µm]')
+ax.set_ylabel('$B_\lambda$ [$W sr^{-1}m^{-2}\mu m^{-1}$]')
+
+ax = axes['b']
+ax.loglog(wl, tot85*bb300, label='8.5 µm')
+ax.fill_between(wl, tot85*bb300, 0, alpha=0.3)
+ax.loglog(wl, bb300, c='k', label='Planck 300 K')
+cumulative_tot85_bb300 = cumulative_trapezoid(tot85 * bb300, wl, initial=None)
+ax.loglog(wl[1:], cumulative_tot85_bb300, label='Cumulative power')
+ax.legend()
+ax.set_xlabel('Wavelength [µm]')
+ax.set_ylabel('$B_\lambda$ [$W sr^{-1}m^{-2}\mu m^{-1}$]')
+
+ax = axes['c']
+ax.loglog(wl, tot185*bb160, label='18.5 µm')
+ax.fill_between(wl, tot185*bb160, 0, alpha=0.3)
+ax.loglog(wl, bb160, c='k', label='Planck 160 K')
+cumulative_tot185_bb160 = cumulative_trapezoid(tot185 * bb160, wl, initial=None)
+ax.loglog(wl[1:], cumulative_tot185_bb160, label='Cumulative power')
+ax.legend()
+ax.set_xlabel('Wavelength [µm]')
+ax.set_ylabel('$B_\lambda$ [$W sr^{-1}m^{-2}\mu m^{-1}$]')
+
+ax = axes['d']
+ax.loglog(wl, tot25*bb40, label='18.5 µm')
+ax.fill_between(wl,  tot25*bb40, 0, alpha=0.3)
+ax.loglog(wl, bb40, c='k', label='Planck 40 K')
+cumulative_tot25_bb40 = cumulative_trapezoid(tot25 * bb40, wl, initial=None)
+ax.loglog(wl[1:], cumulative_tot25_bb40, label='Cumulative power')
+ax.legend()
+ax.set_xlabel('Wavelength [µm]')
+ax.set_ylabel('$B_\lambda$ [$W sr^{-1}m^{-2}\mu m^{-1}$]')
+plt.show()
--- filters/filters.py
+++ filters/filters.py
@@ -1,4 +1,5 @@
-from scipy.signal import interp1d
+from scipy.interpolate import interp1d
+import matplotlib.pyplot as plt
 import scipy.constants as sc
 import numpy as np
 import pandas as pd
@@ -25,49 +26,57 @@ def load_xls(xls, label, wls, in_out=None):
 def load_all_filters():
     wls = np.logspace(-6, -3, 10000)
 
-    wl38, theta38 = load_rawdata('../filters/38um/merged.csv')
+    wl38, theta38 = load_rawdata('filters/38um/merged.csv')
     theta38 = interp1d(wl38, theta38, kind='linear', bounds_error=False, fill_value=(1e-2, 1e-2))(wls)
     theta38[theta38 < 1e-3] = 1e-3
+    x_fir = np.array([70, 90, 100, 200, 300, 400, 500])*1e-6
+    y_fir = np.array([0, .025, .05, .35, .6, .7, .8])
+    irfs_fir = interp1d(x_fir, y_fir, kind='linear', bounds_error=False, fill_value=(0, .8))(wls)
+    theta38 += irfs_fir
 
 
-    wl85, theta85 = load_rawdata('../filters/85um/merged.csv')
+    wl85, theta85 = load_rawdata('filters/85um/merged.csv')
     theta85 = interp1d(wl85, theta85, kind='linear', bounds_error=False, fill_value=(1e-2, .3))(wls)
     theta85[theta85 < 1e-3] = 1e-3
 
-    wl185, theta185 = load_rawdata('../filters/185um/merged.csv')
+    wl185, theta185 = load_rawdata('filters/185um/merged.csv')
     theta185 = interp1d(wl185, theta185, kind='linear', bounds_error=False, fill_value=(1e-2, .3))(wls)
     theta185[theta185 < 1e-3] = 1e-3
 
-    data = pd.read_csv('../filters/CalciumFluoride/CaF2_Uncoated_Trans.csv', header=None, sep=';')
+    data = pd.read_csv('filters/CalciumFluoride/CaF2_Uncoated_Trans.csv', header=None, sep=';')
     wlcaf2 = data[0] / 1E6
     caf2 = data[1] / 1E2
-    caf2 = interp1d(wlcaf2, caf2, kind='linear', bounds_error=False, fill_value=(1e-2, .5))(wls)
+    caf2 = interp1d(wlcaf2, caf2, kind='linear', bounds_error=False, fill_value=(1e-2, 1e-2))(wls)
+    x_fir = np.array([100, 200, 300, 400, 500, 1000])*1e-6
+    y_fir = np.array([0, .07, .14, .28, .35, .56])
+    caf2_fir = interp1d(x_fir, y_fir, kind='linear', bounds_error=False, fill_value=(0, .8))(wls)
+    caf2 += caf2_fir
 
-    folder = '../filters/NeutralDensity/Neutral Density '
+    folder = 'filters/NeutralDensity/Neutral Density '
     data = pd.read_csv(folder + '1.csv', header=None, sep=';')
     wlnd1 = data[0] / 1E9
     nd1 = data[1] / 1E2
-    nd1 = interp1d(wlnd1, nd1, kind='linear', bounds_error=False, fill_value=(1e-2, 1e-2))(wls)
+    nd1 = interp1d(wlnd1, nd1, kind='linear', bounds_error=False, fill_value=(1e-2, np.asarray(nd1)[-1]))(wls)
 
     data = pd.read_csv(folder + '2.csv', header=None, sep=';')
     wlnd2 = data[0] / 1E9
     nd2 = data[1] / 1E2
-    nd2 = interp1d(wlnd2, nd2, kind='linear', bounds_error=False, fill_value=(1e-2, 1e-2))(wls)
+    nd2 = interp1d(wlnd2, nd2, kind='linear', bounds_error=False, fill_value=(1e-2, np.asarray(nd2)[-1]))(wls)
     nd2[np.isnan(nd2)] = 5e-4
 
     data = pd.read_csv(folder + '3.csv', header=None, sep=';')
     wlnd3 = data[0] / 1E9
     nd3 = data[1] / 1E2
-    nd3 = interp1d(wlnd3, nd3, kind='linear', bounds_error=False, fill_value=(1e-3, 1e-3))(wls)
+    nd3 = interp1d(wlnd3, nd3, kind='linear', bounds_error=False, fill_value=(1e-3, np.asarray(nd3)[-1]))(wls)
     nd3[np.isnan(nd3)] = 3e-4
 
-    folder = '../filters/Germanium/Uncoated_ge_window_rawData.csv'
+    folder = 'filters/Germanium/Uncoated_ge_window_rawData.csv'
     data = pd.read_csv(folder, header=None, sep=';')
     wlger = data[0] / 1E9
     ger = data[1] / 1E2
     ger = interp1d(wlger, ger, kind='linear', bounds_error=False, fill_value=(1e-2, .5))(wls)
 
-    folder = '../filters/ZnSe/ZnSe.csv'
+    folder = 'filters/ZnSe/ZnSe.csv'
     data = pd.read_csv(folder, header=None, sep=';')
     wlznse = data[0] / 1E6
     wlznse = wlznse[300:]
@@ -75,15 +84,17 @@ def load_all_filters():
     znse = znse[300:]
     znse = interp1d(wlznse, znse, kind='linear', bounds_error=False, fill_value=(1e-2, 1e-2))(wls)
 
-    path = '../filters/24um/25um filterstack.xlsx'
+    path = 'filters/25um/25um filterstack.xlsx'
     xls = pd.ExcelFile(path)
     filters = []
     labels = ['BP', 'HP300A', 'HP300B', 'LP600']
-    in_outs = [(1e-3, .9), (.1, 1e-3), (.1, 1e-3), (1e-2, .9)]
+    in_outs = [(1e-3, .847), (.1, 1e-3), (.1, 1e-3), (1e-2, .95)]
     for i, label in enumerate(labels):
         filter = load_xls(xls, label, wls, in_outs[i])
         filter[filter<1e-4] = 1e-4
         filters.append(filter)
-    [theta24, SP_A, SP_B, LP] = filters
-    theta24[theta24 < 1e-4] = 1e-4
-    return {'wl':wls, 'bp38':theta38, 'bp85':theta85, 'bp185':theta185, 'caf2':caf2, 'nd1':nd1, 'nd2':nd2, 'nd3':nd3, 'ger':ger, 'znse':znse, 'bp24':theta24, 'sp_a':SP_A, 'sp_b':SP_B, 'lp':LP}
+    [theta25, SP_A, SP_B, LP] = filters
+    theta25[theta25 < 1e-4] = 1e-4
+    return {'wl':wls, 'bp38':theta38, 'bp85':theta85, 'bp185':theta185, 'caf2':caf2, 'nd1':nd1, 'nd2':nd2, 'nd3':nd3, 'ger':ger, 'znse':znse, 'bp25':theta25, 'sp_a':SP_A, 'sp_b':SP_B, 'lp':LP}
+
+# load_all_filters()
